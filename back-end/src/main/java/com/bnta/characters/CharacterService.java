75407bc1482ce37cd4560e5034a552fdc7939fc9
Jake Raffe <jakeraffe24@gmail.com>
2022-03-29 16:19:03 +0100

    Add CharacterDAO methods

--- back-end/src/main/java/com/bnta/characters/CharacterService.java
+++ back-end/src/main/java/com/bnta/characters/CharacterService.java
@@ -29,14 +29,11 @@ public class CharacterService {
     public List<Character> getRandomCharactersByNumber(Integer max) {
         // empty list we will return that will have the random character in it
         List<Character> randomList = new ArrayList<>();
-//        get a random number and use get by id method with this random number
-//        and add character with id to random list
-        //the random list is what we will be returning
-        // check that the id is not already in the list
-        //out database will be assigned to a set
+
         // set the output of get all the characters from out database to a list call allList
         List<Character> allList = characterDAO.getAllCharacters();
-        //creating a set of characters and looping through our allList and adding each character to the set
+        //creating a set of characters and looping through our allList and adding each
+            //  character to the set
         HashSet<Integer> set = new HashSet<>();
         for (Character character: allList
              ) {
@@ -44,71 +41,76 @@ public class CharacterService {
         }
         Random random = new Random();
 
-        // int listLength is the length of the randomList which we want to set to whatever num the pass in as argument
+        // int listLength is the length of the randomList which we want to set to whatever
+            //  num the pass in as argument
         int listLength= 0;
         while(listLength< max){
-            // this is getting a numbers between 1-28 (upper bound 28 is the size of our character database)
+            // this is getting a numbers between 1-28 (upper bound 28 is the size of our
+                // character database)
             int id = 1 + random.nextInt(allList.size());
             //if the set contains the id which has been randomly generated above
             if(set.contains(id)){
-                //then add it to the random list which is what we will later return using the character id which is the id-1 as we need to take away one to refer to the character id rather than the index position
+                //then add it to the random list which is what we will later return using the
+                    // character id which is the id-1 as we need to take away one to refer to
+                    // the character id rather than the index position
                 randomList.add(allList.get(id-1));
-                // removing the id we have just added from the set so that when the next random number we can not call upon the same id
+                // removing the id we have just added from the set so that when the next
+                    // random number we can not call upon the same id
                 set.remove(id);
-                //we then increment the listLength which is external to the while loop and is keeping track of how long the list is
+                //we then increment the listLength which is external to the while loop and
+                    // is keeping track of how long the list is
                 listLength ++;
             }
         }
-// develop comment
-
+        // return list of random chars when loop is fulfilled
         return randomList;
     }
 
     public List<Character> getCharacterByHair(String hair) {
-        return null;
+        return characterDAO.getCharacterByHair(hair);
     }
 
     public List<Character> getCharacterByGender(String gender) {
-        return null;
+        return characterDAO.getCharacterByGender(gender);
     }
 
     public Character getCharacterByName(String name) {
-        return null;
+        return characterDAO.getCharacterByName(name);
     }
 
     public List<Character> getCharacterByEyeColour(String eyeColour) {
-        return null;
+        return characterDAO.getCharacterByEyeColour(eyeColour);
     }
 
     public List<Character> getCharacterByGlasses() {
-        return null;
+        return characterDAO.getCharacterByGlasses();
     }
 
     public List<Character> getCharacterByPiercings() {
-        return null;
+        return characterDAO.getCharacterByPiercings();
     }
 
     public List<Character> getCharacterByBeard() {
-        return null;
+        return characterDAO.getCharacterByBeard();
     }
 
     public List<Character> getCharacterByMoustache() {
-        return null;
+        return characterDAO.getCharacterByMoustache();
     }
 
-    public List<Character> getCharacterByIsLondon() {
-        return null;
+    public List<Character> getCharacterByLondon() {
+        return characterDAO.getCharacterByLondon();
     }
 
     public List<Character> getCharacterByPets() {
-        return null;
+        return characterDAO.getCharacterByPets();
     }
 
     public List<Character> getCharacterByHairAccessory() {
-        return null;
+        return characterDAO.getCharacterByHairAccessory();
     }
 
     public List<Character> getCharacterByTopColour(String topColour) {
-        return null;
+        return characterDAO.getCharacterByTopColour(topColour);
     }
 }
