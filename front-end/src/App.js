5a85b2b74bbcee77f11a064ca3aa8b2b50818ad7
Robyn Garlington <robyn.garlington@hotmail.co.uk>
2022-03-30 12:24:10 +0100

    added state to app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const[characterList, setCharacterList]= useState([]);
+  const[chosenCharacter, setChosenCharacter]= useState({name:"", gender:"", hair_colour:"", eye_colour:"", glasses: false, piercings: false, beard: false, london: false, pets: false, hair_accessory: false, top_colour: ""});
+  const[queryCharacters, setQueryCharacters]= useState([]);
+  const[character, setCharacter]= useState({name:"", gender:"", hair_colour:"", eye_colour:"", glasses: false, piercings: false, beard: false, london: false, pets: false, hair_accessory: false, top_colour: ""});
+
   return (
     <>
     <h1>??Guess Who??</h1>
