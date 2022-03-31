0dbb12619802e4767dd725872a48ba7654618143
Marcy Calderon <marcycalderon@hotmail.co.uk>
2022-03-31 13:35:21 -0400

    add dropdowns for moreOptions

--- front-end/src/components/QuestionForm.js
+++ front-end/src/components/QuestionForm.js
@@ -1,37 +1,141 @@
 import { useState } from "react"
+
 const QuestionForm = ({}) => {
 
+    // follow up options to filter characters
     const [moreOptions, setMoreOptions] = useState([])
 
     const handleMoreOptions = (event) => {
         // gets the value that has been selected 
 
         //ToDo: add if else statement for different questions and their diff dropdown options
-        
-        console.log(event.target.value);
-        // setMoreOptions(
-        // <form>
-        // <select name="hairDropDown">
-        //     <option value="blonde">Blonde</option>
-        //     <option value="brown">Brown</option>
-        //     <option value="black">Black</option>
-        //     <option value="hijab">Hijab</option>
-        // </select>
-        // </form> 
-        // )
+        if (event.target.value === 'hair'){
+            setMoreOptions(
+                <form>
+                <select name="hairDropDown">
+                    <option value="blonde">Blonde</option>
+                    <option value="brown">Brown</option>
+                    <option value="black">Black</option>
+                    <option value="hijab">Hijab</option>
+                </select>
+                </form> 
+                )
+        }
+        else if (event.target.value === 'top'){
+            setMoreOptions(
+                <form>
+                <select name="topDropDown">
+                    <option value="black">Black</option>
+                    <option value="white">White</option>
+                    <option value="red">Red</option>
+                </select>
+                </form> 
+                )
+        } 
+        else if (event.target.value === 'eyes'){
+            setMoreOptions(
+                <form>
+                <select name="eyesDropDown">
+                    <option value="blue">Blue</option>
+                    <option value="brown">Brown</option>
+                    <option value="green">Green</option>
+                </select>
+                </form> 
+                )
+        }
+        else if (event.target.value === 'gender'){
+            setMoreOptions(
+                <form>
+                <select name="genderDropDown">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                </form> 
+                )
+        }
+        else if (event.target.value === 'piercings'){
+            setMoreOptions(
+                <form>
+                <select name="piercingsDropDown">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </select>
+                </form> 
+                )
+        } 
+        else if (event.target.value === 'beard'){
+            setMoreOptions(
+                <form>
+                <select name="beardDropDown">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </select>
+                </form> 
+                )
+        } 
+        else if (event.target.value === 'london'){
+            setMoreOptions(
+                <form>
+                <select name="londonDropDown">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </select>
+                </form> 
+                )
+        } 
+        else if (event.target.value === 'pets'){
+            setMoreOptions(
+                <form>
+                <select name="petsDropDown">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </select>
+                </form> 
+                )
+        } 
+        else if (event.target.value === 'hair_accessory'){
+            setMoreOptions(
+                <form>
+                <select name="hair_accessoryDropDown">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </select>
+                </form> 
+                )
+        } 
+        else if (event.target.value === 'glasses'){
+            setMoreOptions(
+                <form>
+                <select name="glassesDropDown">
+                    <option value="true">True</option>
+                    <option value="false">False</option>
+                </select>
+                </form> 
+                )
+        } 
     }
    
 
     return (
+        <>
         <form>
             {/* When option is selected, run method to create more options depending on what's been selected */}
         <select name ="questions" onChange = {(event) => handleMoreOptions(event)} >
+            <option value="">Select from the options below...</option>
             <option value="hair" >Is their hair:</option> 
             <option value="top">Is their top:</option>
             <option value="eyes">Are their eyes:</option>
+            <option value="gender">Is the person:</option>
+            <option value="piercings">Do they have piercings:</option>
+            <option value="beard">Do they have a beard:</option>
+            <option value="london">Do they live in London:</option>
+            <option value="pets">Do they have pets:</option>
+            <option value="hair_accessory">Do they have a hair accessory:</option>
+            <option value="glasses">Do they wear glasses:</option>
         </select>
-        {moreOptions}
         </form>
+        {moreOptions}
+        </>
     )
 }
 
