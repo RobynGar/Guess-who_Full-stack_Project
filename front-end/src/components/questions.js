b90046043b06541e82918a72a744cf4b10d250c8
Alimaa <alimaahashim@gmail.com>
2022-04-01 12:53:12 +0100

    add dropdown options

--- front-end/src/components/questions.js
+++ front-end/src/components/questions.js
@@ -3,7 +3,7 @@ export const questions = [{
         title: "Questions",
         submenu:[
             {
-                title: "Is their hair: ",
+                title: "Is their hair colour...",
                 submenu: [
                     {title: "blonde",},
                     {title: "black",},
@@ -14,7 +14,7 @@ export const questions = [{
             },
 
             {
-                title: "Is their top: ",
+                title: "Is their top colour...",
                 submenu: [
                     {title: "red",},
                     {title: "white",},
@@ -24,19 +24,50 @@ export const questions = [{
 
             },
             {
-                title: "Are their eyes: ",
+                title: "Are their eyes...",
                 submenu: [
                     {title: "brown",},
                     {title: "blue",},
                     {title: "green",},
 
                 ],
-            }
+            },
+            {
+                title: "Gender...",
+                submenu: [
+                    {title: "male",},
+                    {title: "female",},
+
+                ],
+            },
+            {
+                title: "Do they have a piercing?",
+                
+            },
+            {
+                title: "Do they have a beard?",
+                
+            },
+            {
+                title: "Do they live in London?",
+                
+            },
+            {
+                title: "Do they have pets?",
+                
+            },
+            {
+                title: "Do they have a hair accessory?",
+                
+            },
+            {
+                title: "Do they wear glasses?",
+                
+            },
+
             
         ]
     }];
-
-
 
 
 
